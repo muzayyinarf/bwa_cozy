1c847b13a9fb974fa96644abece9d3b26ed709e4
Muzayyin Arifin <muzayyinarifin46@gmail.com>
2022-10-10 15:19:53 +0700

    first commit

---
+++ lib/models/space.dart
@@ -0,0 +1,51 @@
+class Space {
+  int id;
+  String name;
+  String imageUrl;
+  int price;
+  String city;
+  String country;
+  int rating;
+  String address;
+  String phone;
+  String mapUrl;
+  List photos;
+  int numberOfKitchens;
+  int numberOfBedrooms;
+  int numberOfCupboards;
+
+  Space(
+      {required this.id,
+      required this.name,
+      required this.imageUrl,
+      required this.price,
+      required this.city,
+      required this.country,
+      required this.rating,
+      required this.address,
+      required this.phone,
+      required this.mapUrl,
+      required this.photos,
+      required this.numberOfKitchens,
+      required this.numberOfBedrooms,
+      required this.numberOfCupboards});
+
+  factory Space.fromJson(Map<String, dynamic> json) {
+    return Space(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['image_url'],
+      price: json['price'],
+      city: json['city'],
+      country: json['country'],
+      rating: json['rating'],
+      address: json['address'],
+      phone: json['phone'],
+      mapUrl: json['map_url'],
+      photos: json['photos'],
+      numberOfKitchens: json['number_of_kitchens'],
+      numberOfBedrooms: json['number_of_bedrooms'],
+      numberOfCupboards: json['number_of_cupboards'],
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,216 @@
+import 'package:bwa_cozy/models/city.dart';
+import 'package:bwa_cozy/models/space.dart';
+import 'package:bwa_cozy/models/tips.dart';
+import 'package:bwa_cozy/providers/space_provider.dart';
+import 'package:bwa_cozy/widgets/bottom_navbar.dart';
+import 'package:bwa_cozy/widgets/city_card.dart';
+import 'package:bwa_cozy/widgets/space_card.dart';
+import 'package:bwa_cozy/widgets/tips_card.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../theme.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var spaceProvider = Provider.of<SpaceProvider>(context);
+
+    return Stack(
+      children: [
+        Container(color: whiteColor,),
+        SafeArea(
+          child: Scaffold(
+            body: ListView(
+              children: [
+                //NOTE : TITLE/HEADER
+                Padding(
+                  padding: EdgeInsets.only(left: edge, top: edge),
+                  child: Text(
+                    "Explore Now",
+                    style: blackTextStyle.copyWith(fontSize: 24),
+                  ),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: edge),
+                  child: Text(
+                    "Mencari kosan yang cozy",
+                    style: greyTextStyle.copyWith(fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //NOTE : POPULAR CITIES
+                Padding(
+                  padding: EdgeInsets.only(left: edge),
+                  child: Text(
+                    "Popular Cities",
+                    style: regularTextStyle.copyWith(fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      SizedBox(
+                        width: 24,
+                      ),
+                      CityCard(City(
+                          id: 1, name: "Jakarta", imageUrl: 'assets/city1.png')),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CityCard(City(
+                          id: 2,
+                          name: "Bandung",
+                          imageUrl: 'assets/city2.png',
+                          isPopular: true)),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CityCard(City(
+                          id: 1, name: "Surabaya", imageUrl: 'assets/city3.png')),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CityCard(City(
+                          id: 1, name: "Palembang", imageUrl: 'assets/city4.png')),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CityCard(City(
+                          id: 1,
+                          name: "Aceh",
+                          imageUrl: 'assets/city5.png',
+                          isPopular: true)),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      CityCard(
+                          City(id: 1, name: "Bogor", imageUrl: 'assets/city6.png')),
+                      SizedBox(
+                        width: 24,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //NOTE : RECOMMENDED SPACE
+                Padding(
+                  padding: EdgeInsets.only(left: edge),
+                  child: Text(
+                    "Recommended Space",
+                    style: regularTextStyle.copyWith(fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: edge),
+                  child: FutureBuilder(
+                    future: spaceProvider.getRecommendedSpaces(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        List<Space> data = snapshot.data as List<Space>;
+
+                        int index = 0;
+
+                        return Column(
+                          children: data.map((item) {
+                            index++;
+                            return Container(
+                              margin: EdgeInsets.only(
+                                top: index == 1 ? 0 : 30,
+                              ),
+                              child: SpaceCard(item),
+                            );
+                          }).toList(),
+                        );
+                      } else {
+                        return Center(child: CircularProgressIndicator(color: purpleColor,));
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //NOTE : TIPS & GUIDANCE
+                Padding(
+                  padding: EdgeInsets.only(left: edge),
+                  child: Text(
+                    "Tips & Guidance",
+                    style: regularTextStyle.copyWith(fontSize: 16),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: edge),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TipsCard(Tips(
+                          id: 1,
+                          title: 'City Guidelines',
+                          updatedAt: '8 August',
+                          imageUrl: 'assets/tips1.png')),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TipsCard(Tips(
+                          id: 1,
+                          title: 'Jakarta Farship',
+                          updatedAt: '8 August',
+                          imageUrl: 'assets/tips2.png')),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 90,
+                ),
+              ],
+            ),
+            floatingActionButton: Container(
+              height: 65,
+              width: MediaQuery.of(context).size.width - (2 * edge),
+              margin: EdgeInsets.symmetric(horizontal: edge),
+              decoration: BoxDecoration(
+                  color: Color(0xffF6F7F8),
+                  borderRadius: BorderRadius.circular(23)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  BottomNavbarItem(
+                      imageUrl: 'assets/icon_home.png', isActive: true),
+                  BottomNavbarItem(
+                      imageUrl: 'assets/icon_email.png', isActive: false),
+                  BottomNavbarItem(
+                      imageUrl: 'assets/icon_card.png', isActive: false),
+                  BottomNavbarItem(
+                      imageUrl: 'assets/icon_love.png', isActive: false),
+                ],
+              ),
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          ),
+        ),
+      ],
+    );
+  }
+}
